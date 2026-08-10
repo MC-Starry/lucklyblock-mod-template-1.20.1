05676e34bceb6a536dcaaa3a6ba3d8417694c2cb
Administrator <2905586376@qq.com>
2026-08-10 23:23:54 +0800

    Initial commit

---
+++ src/client/java/fun/starry/client/LucklyBlockModClient.java
@@ -0,0 +1,10 @@
+package fun.starry.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LucklyBlockModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
